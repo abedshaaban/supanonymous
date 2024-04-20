5bde05ba3ca9f6929f3d9ab70373aa146fa0447d
Abed Al Ghani Mosbah Shaaban <abedshaaban600@gmail.com>
2024-04-20 21:04:19 +0300

    refactor: created constants folder and import alias

---
+++ src/lib/constants/site-config.ts
@@ -0,0 +1,8 @@
+export const siteConfig = {
+  name: 'Supanonymous',
+  socialLinks: {
+    github: 'https://github.com/abedshaaban/supanonymous'
+  }
+}
+
+export type SiteConfig = typeof siteConfig
