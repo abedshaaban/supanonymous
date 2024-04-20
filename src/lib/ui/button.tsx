efe053e992c0375740e3eb0e6ae26b03bae1e520
Abed Al Ghani Mosbah Shaaban <abedshaaban600@gmail.com>
2024-04-20 21:56:42 +0300

    refactor: updated button outline variant

--- src/lib/ui/button.tsx
+++ src/lib/ui/button.tsx
@@ -5,13 +5,14 @@ import { cn } from '@utils/index'
 import { cva, type VariantProps } from 'class-variance-authority'
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
+  'duration-150 transition-all inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       variant: {
         default: 'bg-primary text-white fill-white shadow hover:bg-primary/90',
         destructive: 'bg-red text-white fill-white shadow-sm hover:bg-red/90',
-        outline: 'border text-black border-input bg-background shadow-sm',
+        outline:
+          'border text-black border-input bg-background shadow-sm hover:bg-gray-100',
         secondary:
           'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80',
         ghost:
