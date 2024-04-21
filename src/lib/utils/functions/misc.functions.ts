d1867fec05ef625d5a3ba0a93a5d5d1791a07249
Abed Al Ghani Mosbah Shaaban <abedshaaban600@gmail.com>
2024-04-22 00:14:06 +0300

    refactor: generate id

--- src/lib/utils/functions/misc.functions.ts
+++ src/lib/utils/functions/misc.functions.ts
@@ -1,4 +1,5 @@
 import clsx, { type ClassValue } from 'clsx'
+import { nanoid } from 'nanoid'
 import { twMerge } from 'tailwind-merge'
 import clip from 'text-clipper'
 
@@ -8,4 +9,8 @@ export function shortenText(text: string, maxLength: number, options?: object): 
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function generateId(size = 9): string {
+  return nanoid(size)
 }
