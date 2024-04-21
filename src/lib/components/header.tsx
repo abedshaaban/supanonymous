905960df8ba7a80dd065686266f7bd6fa65ec996
Abed Al Ghani Mosbah Shaaban <abedshaaban600@gmail.com>
2024-04-21 14:35:28 +0300

    refactor: updated header

--- src/lib/components/header.tsx
+++ src/lib/components/header.tsx
@@ -1,9 +1,17 @@
 import { siteConfig } from '~constants/site-config'
 import CircleIcon from '~icons/circle'
+import ExitIcon from '~icons/exit'
+import PersonIcon from '~icons/person'
 import { Button } from '~ui/button'
+import { logoutUser } from '~utils/client/auth-actions'
+import { supabase } from '~utils/supabase/server'
 import Link from 'next/link'
 
-export function Header() {
+export async function Header() {
+  const {
+    data: { user }
+  } = await supabase.auth.getUser()
+
   return (
     <header
       className={
@@ -14,7 +22,7 @@ export function Header() {
         <div className={'mr-4'}>
           <Link href="/" className={'mr-6 flex items-center space-x-2'}>
             <CircleIcon className={'h-6 w-6'} />
-            <span className={'text-primary hidden font-bold md:inline-block'}>
+            <span className={'hidden font-bold text-primary md:inline-block'}>
               {siteConfig.name}
             </span>
           </Link>
@@ -22,9 +30,29 @@ export function Header() {
 
         <div className={'flex flex-1 items-center justify-end space-x-2'}>
           <nav className={'flex items-center gap-3'}>
-            <Button variant={'outline'}>Login</Button>
+            {user ? (
+              <>
+                <Link href={'/u'}>
+                  <Button variant={'outline'} size={'icon'}>
+                    <PersonIcon />
+                  </Button>
+                </Link>
 
-            <Button>Sign up</Button>
+                <Button variant={'destructive'} onClick={logoutUser} size={'icon'}>
+                  <ExitIcon />
+                </Button>
+              </>
+            ) : (
+              <>
+                <Link href={'/login'}>
+                  <Button variant={'outline'}>Login</Button>
+                </Link>
+
+                <Link href={'/signup'}>
+                  <Button>Sign up</Button>
+                </Link>
+              </>
+            )}
           </nav>
         </div>
       </div>
