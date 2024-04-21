7f755950952951aed7debc8b06c7aeb40da6db72
Abed Al Ghani Mosbah Shaaban <abedshaaban600@gmail.com>
2024-04-22 01:48:14 +0300

    refactor: added slasj icon

---
+++ src/lib/icons/slash.tsx
@@ -0,0 +1,19 @@
+import type { IconType } from '~types/icons'
+
+const SlashIcon = (props: IconType) => (
+  <svg
+    width="15"
+    height="15"
+    viewBox="0 0 15 15"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M4.10876 14L9.46582 1H10.8178L5.46074 14H4.10876Z"
+      fillRule="evenodd"
+      clipRule="evenodd"
+    />
+  </svg>
+)
+
+export default SlashIcon
