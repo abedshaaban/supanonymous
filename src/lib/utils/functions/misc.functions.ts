0398cd643c2323493c3363ef6263c8b5a5f9549f
Abed Al Ghani Mosbah Shaaban <abedshaaban600@gmail.com>
2024-04-21 15:04:16 +0300

    refactor: updated function

--- src/lib/utils/functions/misc.functions.ts
+++ src/lib/utils/functions/misc.functions.ts
@@ -1,5 +1,10 @@
 import clsx, { type ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
+import clip from 'text-clipper'
+
+export function shortenText(text: string, maxLength: number, options?: object): string {
+  return clip(text, maxLength, options)
+}
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
