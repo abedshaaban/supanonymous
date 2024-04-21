c4f7aac98324641aa46492c646247d31eb82f66a
Abed Al Ghani Mosbah Shaaban <abedshaaban600@gmail.com>
2024-04-21 14:34:58 +0300

    refactor: updated home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
-export default function HomePage() {
-  return <div>Hello world</div>
+import { supabase } from '~utils/supabase/server'
+
+export default async function HomePage() {
+  const {
+    data: { user }
+  } = await supabase.auth.getUser()
+
+  return <div>Hello {user ? user.email : 'world'}</div>
 }
